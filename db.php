4af79541ca790f0d37ae0dd1438f068ba3e5aad2
vas-1807 <89392993+vas-1807@users.noreply.github.com>
2022-08-22 16:41:05 +0530

    Delete db.php

--- db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$sname="localhost";
-$rname="root";
-$spassword="";
-$db_name="registration";
-$conn=mysqli_connect($sname,$rname,$spassword,$db_name);
-if(!$conn){
-    echo "connection Failed!";
-}
